1cb8f2ffe3732be6ecd9f8745c0ab52a3d08fc1a
Andre Susilo <andresusilo279@gmail.com>
2025-01-17 23:21:17 +0700

    Perbaikan Integrasi Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:artshift/firebase_options.dart';
 import 'package:artshift/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
